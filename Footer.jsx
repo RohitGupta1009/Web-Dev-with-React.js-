e5957adb2f02468e840af9befd588b6427eedf40
RohitGupta1009 <83397363+RohitGupta1009@users.noreply.github.com>
2021-06-05 23:14:24 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Footer() {
+  var CurrYear = new Date().getFullYear();
+  return (
+    <footer>
+      <p> Copyright @ {CurrYear} </p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1> Javascript and React.js </h1>
+      <p>
+        This was an amazing bootcamp conducted by shaurya sir. We cover
+        everything from scratch including Javascript,React.js, HTML and CSS.
+      </p>
+    </div>
+  );
+}
+export default Note;
